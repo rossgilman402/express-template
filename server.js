7fe2562931a24296e75c4cc112f1b44ef1bff571
ross402 <65184393+ross402@users.noreply.github.com>
2023-09-21 14:51:06 -0400

    Adds default route

--- server.js
+++ server.js
@@ -8,11 +8,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
 
 //ROUTES
+app.get("*", (req, res) => {
+  res.send("You need to add some routes");
+});
 
 //START THE SERVER
-app.listen(PORT),
-  () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  };
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
